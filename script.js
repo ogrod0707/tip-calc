e283519d02f3fb5507294e89d80930288b02e6bd
Krystian O <ogrod0808@gmail.com>
2021-08-25 20:07:07 +0200

    added some lines of js, still unfinished

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
+//dane pobierane z buttonow oraz inputow
+const billInput = document.querySelector(".money-input");
+const tipBtn = document.querySelectorAll(".btn-tip");
+const customTipInput = document.querySelector(".custom");
+const peopleInput = documen.querySelector(".people-number");
+const tipAmount = document.querySelector(".tip-amount");
+const totalAmount = documen.querySelector(".total-amount");
+const resetBtn = document.querySelector(".reset");
 
+let bill;
+let tip = 0;
+let customTip = 0;
+let peopleNum;
+const tipBtnArr = Array.from(tipBtn);
+peopleInput.value = "";
 
+const removeActive = function (arr) {
+    arr
+      .find((el) => el.classList.contains("btn-tip--active"))
+      ?.classList.remove("btn-tip--active");
+  };
 
-document.querySelector('.tips').addEventListener("click", function(event) {
-    buttonClick(event.target.value);
-    console.log("here")
-})
-
+  const displayBill = function () {
+    if (bill && peopleNum && (tip || customTip)) {
+      let currentTip;
+      if (
+        tipBtnArr.some((tipBtn) =>
+          tipBtn.classList.contains("btn-tip--active")
+        )
+      ) {
+        currentTip = tip;
+      } else {
+        currentTip = customTip;
+      }
+      tipAmount.textContent = ((bill * currentTip) / peopleNum).toFixed(2) + "$";
+      totalAmount.textContent =
+        (bill / peopleNum + parseFloat(tipAmount.textContent)).toFixed(2) + "$";
+    }
+  };
+  billInput.addEventListener("blur", function () {
+    bill = +billInput.value;
+    displayBill();
+  });
