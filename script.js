dbff7114530facc9f4a4c8cbe1efdcb82e24ebaa
GimmeSomeCoffee <80617954+ogrod0707@users.noreply.github.com>
2021-08-25 20:17:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+//dane pobierane z buttonow oraz inputow
+const billInput = document.querySelector(".money-input");
+const tipBtn = document.querySelectorAll(".btn-tip");
+const customTipInput = document.querySelector(".custom");
+const peopleInput = documen.querySelector(".people-number");
+const tipAmount = document.querySelector(".tip-amount");
+const totalAmount = documen.querySelector(".total-amount");
+const resetBtn = document.querySelector(".reset");
 
-// function calc(){
+let bill;
+let tip = 0;
+let customTip = 0;
+let peopleNum;
+const tipBtnArr = Array.from(tipBtn);
+peopleInput.value = "";
 
-//     let tip5 = document.querySelector('input[name="5"');
-//     let tip10 = docu
+const removeActive = function (arr) {
+    arr
+      .find((el) => el.classList.contains("btn-tip--active"))
+      ?.classList.remove("btn-tip--active");
+  };
 
-
-
-// }
+  const displayBill = function () {
+    if (bill && peopleNum && (tip || customTip)) {
+      let currentTip;
+      if (
+        tipBtnArr.some((tipBtn) =>
+          tipBtn.classList.contains("btn-tip--active")
+        )
+      ) {
+        currentTip = tip;
+      } else {
+        currentTip = customTip;
+      }
+      tipAmount.textContent = ((bill * currentTip) / peopleNum).toFixed(2) + "$";
+      totalAmount.textContent =
+        (bill / peopleNum + parseFloat(tipAmount.textContent)).toFixed(2) + "$";
+    }
+  };
+  billInput.addEventListener("blur", function () {
+    bill = +billInput.value;
+    displayBill();
+  });
